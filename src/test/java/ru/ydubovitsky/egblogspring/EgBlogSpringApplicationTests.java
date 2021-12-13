d37e7b0416808994f008012b1a19129dd7608837
user <y.dubovitsky@gmail.com>
2021-12-13 13:57:50 +0300

    init commit

---
+++ src/test/java/ru/ydubovitsky/egblogspring/EgBlogSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ydubovitsky.egblogspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgBlogSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
