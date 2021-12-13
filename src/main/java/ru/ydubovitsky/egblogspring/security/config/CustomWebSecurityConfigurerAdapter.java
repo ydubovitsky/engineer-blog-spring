31accb4e41e2002e2689d0c715aad130043518d6
user <y.dubovitsky@gmail.com>
2021-12-13 17:53:28 +0300

    added a class extending WebSecurityConfigurerAdapter and realization of UsernamePasswordAuthenticationFilter, witch generates and return to user JWT if user exists in database (temporary MOCK)

---
+++ src/main/java/ru/ydubovitsky/egblogspring/security/config/CustomWebSecurityConfigurerAdapter.java
@@ -0,0 +1,36 @@
+package ru.ydubovitsky.egblogspring.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.ydubovitsky.egblogspring.security.filter.JwtUsernameAndPasswordAuthFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class CustomWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter(new JwtUsernameAndPasswordAuthFilter(authenticationManager()))
+                .authorizeRequests()
+                .antMatchers("/api/post/add").permitAll();
+    }
+}
---
+++ src/main/java/ru/ydubovitsky/egblogspring/security/filter/JwtUsernameAndPasswordAuthFilter.java
@@ -0,0 +1,68 @@
+package ru.ydubovitsky.egblogspring.security.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import ru.ydubovitsky.egblogspring.security.request.UsernameAndPasswordAuthRequest;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Date;
+
+public class JwtUsernameAndPasswordAuthFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+    private final String key = "org.springframework.boot.devtools.restrt.classloader.RestartClassLoaderorg.springframework.boot.devtools.restart.classloader.RestartClassLoader";
+
+    public JwtUsernameAndPasswordAuthFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws AuthenticationException {
+        try {
+            UsernameAndPasswordAuthRequest req = new ObjectMapper()
+                    .readValue(request.getInputStream(), UsernameAndPasswordAuthRequest.class);
+
+            Authentication authentication = new UsernamePasswordAuthenticationToken(
+                    req.getUsername(),
+                    req.getPassword(),
+                    null);
+
+            return authenticationManager.authenticate(authentication);
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain chain,
+            Authentication authResult
+    ) throws IOException, ServletException {
+        String token = Jwts.builder()
+                .setSubject(authResult.getName())
+                .claim("authorities", authResult.getAuthorities())
+                .setIssuedAt(new Date())
+                //FIXME?
+                .setExpiration(new java.sql.Date(System.currentTimeMillis() + LocalDate.now().plusWeeks(4).toEpochDay()))
+                .signWith(Keys.hmacShaKeyFor(key.getBytes()))
+                .compact();
+
+        response.addHeader("Authorization", "Bearer " + token);
+    }
+}
