2bfe234010225f85867d170716f430734a96c762
user <y.dubovitsky@gmail.com>
2021-12-14 13:13:18 +0300

    fixed bug in method signature (ImageRepository)

--- src/main/java/ru/ydubovitsky/egblogspring/repository/ImageRepository.java
+++ src/main/java/ru/ydubovitsky/egblogspring/repository/ImageRepository.java
@@ -3,5 +3,5 @@ package ru.ydubovitsky.egblogspring.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.ydubovitsky.egblogspring.entity.Image;
 
-public interface ImageRepository extends JpaRepository<Integer, Image> {
+public interface ImageRepository extends JpaRepository<Image, Integer> {
 }
