7c6920e87340bdd2c6355df1b3882ce4665a0b80
user <y.dubovitsky@gmail.com>
2022-04-08 16:29:52 +0300

    Added utils for compression bytes.

---
+++ src/main/java/ru/ydubovitsky/engineerBlog/utils/CompressionUtils.java
@@ -0,0 +1,51 @@
+package ru.ydubovitsky.engineerBlog.utils;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.zip.DataFormatException;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
+
+@Slf4j
+public class CompressionUtils {
+
+    public static byte[] compress(byte[] data) throws IOException {
+        Deflater deflater = new Deflater();
+        deflater.setInput(data);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+
+        deflater.finish();
+        byte[] buffer = new byte[1024];
+        while (!deflater.finished()) {
+            int count = deflater.deflate(buffer); // returns the generated code... index
+            outputStream.write(buffer, 0, count);
+        }
+        outputStream.close();
+        byte[] output = outputStream.toByteArray();
+
+        log.debug("Original: " + data.length / 1024 + " Kb");
+        log.debug("Compressed: " + output.length / 1024 + " Kb");
+        return output;
+    }
+
+    public static byte[] decompress(byte[] data) throws IOException, DataFormatException {
+        Inflater inflater = new Inflater();
+        inflater.setInput(data);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+        byte[] buffer = new byte[1024];
+        while (!inflater.finished()) {
+            int count = inflater.inflate(buffer);
+            outputStream.write(buffer, 0, count);
+        }
+        outputStream.close();
+        byte[] output = outputStream.toByteArray();
+
+        log.debug("Original: " + data.length);
+        log.debug("Compressed: " + output.length);
+        return output;
+    }
+}
