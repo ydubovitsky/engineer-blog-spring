78ee99f044432f90dba70a2948a5fedb8ba7ac48
user <y.dubovitsky@gmail.com>
2023-09-15 16:07:41 +0300

    CORSConfig updated; List of allowed methods added

--- src/main/java/ru/ydubovitsky/engineerBlog/security/config/CORSConfig.java
+++ src/main/java/ru/ydubovitsky/engineerBlog/security/config/CORSConfig.java
@@ -1,13 +1,14 @@
 package ru.ydubovitsky.engineerBlog.security.config;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import ru.ydubovitsky.engineerBlog.config.VariablesConfig;
 
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class CORSConfig {
@@ -20,7 +21,7 @@ public class CORSConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedMethods("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedOrigins(variablesConfig.getAllowedOriginsArray().split(","));
             }
         };
